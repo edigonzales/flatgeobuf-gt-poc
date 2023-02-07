f881417adfc5754be6360658aa0feeb322302190
edigonzales <edi.gonzales@gmail.com>
2023-02-07 20:28:27 +0100

    wip

--- app/src/main/java/ch/so/agi/App.java
+++ app/src/main/java/ch/so/agi/App.java
@@ -3,12 +3,62 @@
  */
 package ch.so.agi;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.flatgeobuf.FlatGeobufDataStoreFactory;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.simple.SimpleFeatureStore;
+import org.opengis.feature.simple.SimpleFeatureType;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+
+        Map<String,Object> params = new HashMap();
+        params.put("dbtype", "geopkg");
+        params.put("database", "src/test/data/ch.so.agi.av_gb_administrative_einteilungen.gpkg");
+        params.put("read-only", true);
+
+        DataStore datastore = DataStoreFinder.getDataStore(params);
+        
+        System.out.println(datastore.getTypeNames());
+        
+        for (String typeName : datastore.getTypeNames()) {
+            System.out.println(typeName);
+        }
+        
+        
+        SimpleFeatureSource fs = datastore.getFeatureSource("grundbuchkreise_grundbuchkreis");
+        SimpleFeatureCollection fc = fs.getFeatures();
+        System.out.println(fc.getBounds());
+    
+        File fgbFile = new File("/Users/stefan/tmp/abbaustelle.fgb");
+        Map<String, Serializable> fgbParams = new HashMap<>();
+        fgbParams.put("url", fgbFile.toURI().toURL());
+        FlatGeobufDataStoreFactory dataStoreFactory = new FlatGeobufDataStoreFactory();
+        DataStore fgbDatastore = dataStoreFactory.createDataStore(fgbParams);
+    
+        SimpleFeatureType featureType = fc.getSchema();
+        System.out.println(featureType.getCoordinateReferenceSystem());
+        
+        
+        
+        
+        fgbDatastore.createSchema(featureType);
+  
+        System.out.println(fgbDatastore.getTypeNames()[0]);
+        
+        
+        SimpleFeatureStore featureStore = (SimpleFeatureStore) fgbDatastore.getFeatureSource("abbaustelle");
+        featureStore.addFeatures(fc);
+
+    
     }
 }
